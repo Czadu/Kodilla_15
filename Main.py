05e32997c9cf7caf2b6ee46fe4d7e99c19127df2
Cezary Grzesiuk <cezary.grzesiuk@gmail.com>
2023-03-11 14:42:00 +0100

    init_proj

---
+++ Main.py
@@ -0,0 +1,14 @@
+shopping_list = {
+            "piekarnia" : ["Chleb", "Paczek", "Bulki"],
+            "warzywniak" : ['Marchew', 'Seler', 'Rukola'],
+            "biedronka" : ['Ser', 'Maslo']
+}
+
+x = 0
+print()
+for shop, items in shopping_list.items():
+    print(f"idę do {shop.capitalize()} i kupuję {items}" "\n")
+    for items in shopping_list[shop]:
+        x += 1
+        
+print (f"w sumie kupuję {x} produktów")
