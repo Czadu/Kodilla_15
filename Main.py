f44e551518684a647e1c8c8265184f215900ae51
Cezary Grzesiuk <cezary.grzesiuk@gmail.com>
2023-03-11 14:47:24 +0100

    capitalize

--- Main.py
+++ Main.py
@@ -2,7 +2,7 @@ shopping_list = {
             "piekarnia" : ["Chleb", "Paczek", "Bulki"],
             "warzywniak" : ['Marchew', 'Seler', 'Rukola'],
             "biedronka" : ['Ser', 'Maslo'],
-            "lidl" : ['szynka', 'jogurt']
+            "lidl" : ['Szynka', 'Jogurt']
 }
 
 x = 0
